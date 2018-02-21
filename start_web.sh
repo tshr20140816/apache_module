c3e1ee93eca30baa24ac6627d95e548e2b3103dc
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-22 01:40:35 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -7,10 +7,12 @@ export TZ=JST-9
 ls -lang
 mkdir -p /tmp/usr/lib
 cp libnghttp2.so.14 /tmp/usr/lib/
+cp libbrotlicommon.so.1 /tmp/usr/lib/
 cp libbrotlienc.so.1 /tmp/usr/lib/
 
 ls -lang /tmp/usr/lib/
 
+ldd /tmp/usr/lib/libbrotlicommon.so.1
 ldd /tmp/usr/lib/libbrotlienc.so.1
 
 ldd ./mod_http2.so
