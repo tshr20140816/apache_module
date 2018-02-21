57cb82bf0c42473be025b7b38650efc2727998f3
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-22 01:34:45 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -9,6 +9,10 @@ mkdir -p /tmp/usr/lib
 cp libnghttp2.so.14 /tmp/usr/lib/
 cp libbrotlienc.so.1 /tmp/usr/lib/
 
+ls -lang /tmp/usr/lib/
+
+ldd /tmp/usr/lib/libbrotlienc.so.1
+
 ldd ./mod_http2.so
 ldd ./mod_proxy_http2.so
 ldd ./mod_brotli.so
