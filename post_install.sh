b7591ab7d4298eca8b667775ca748edd0366f1ee
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-22 17:15:02 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -27,25 +27,6 @@ ALTER TABLE t_files ADD CONSTRAINT table_key PRIMARY KEY(file_name);
 __HEREDOC__
 cat /tmp/sql_result.txt
 
-psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
-SELECT file_base64_text
-  FROM t_files
- WHERE file_name = 'dummy'
-__HEREDOC__
-cat /tmp/sql_result.txt
-
-psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
-INSERT INTO t_files (file_name, file_base64_text) VALUES ('dummy1', '1234567890');
-__HEREDOC__
-cat /tmp/sql_result.txt
-
-psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
-SELECT file_base64_text
-  FROM t_files
- WHERE file_name = 'dummy'
-__HEREDOC__
-cat /tmp/sql_result.txt
-
 date
 start_date=$(date)
 
@@ -62,13 +43,13 @@ cd /tmp
 openssl version
 
 wget https://c-ares.haxx.se/download/c-ares-1.13.0.tar.gz &
-# wget http://www.digip.org/jansson/releases/jansson-2.11.tar.bz2 &
-# wget https://github.com/nghttp2/nghttp2/releases/download/v1.30.0/nghttp2-1.30.0.tar.xz &
-# wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.tar.gz &
-# git clone --depth 1 https://github.com/google/brotli &
-# wget http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-1.6.3.tar.bz2 &
-# wget http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-util-1.6.1.tar.bz2 &
-# wget http://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.29.tar.gz &
+wget http://www.digip.org/jansson/releases/jansson-2.11.tar.bz2 &
+wget https://github.com/nghttp2/nghttp2/releases/download/v1.30.0/nghttp2-1.30.0.tar.xz &
+wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.tar.gz &
+git clone --depth 1 https://github.com/google/brotli &
+wget http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-1.6.3.tar.bz2 &
+wget http://ftp.tsukuba.wide.ad.jp/software/apache//apr/apr-util-1.6.1.tar.bz2 &
+wget http://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.29.tar.gz &
 
 
 wget https://www.samba.org/ftp/ccache/ccache-3.3.4.tar.gz
