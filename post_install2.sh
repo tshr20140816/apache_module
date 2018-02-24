3f4ba8f1e6f627253fb075e7d91da038690aca0e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-24 14:50:15 +0900

    Update post_install2.sh

--- post_install2.sh
+++ post_install2.sh
@@ -7,9 +7,9 @@ start_date=$(date)
 
 cd /tmp
 
-wget https://tukaani.org/xz/xz-5.2.3.tar.bz2
+wget https://tukaani.org/xz/xz-5.2.3.tar.gz
 
-tar xf xz-5.2.3.tar.bz2
+tar xf xz-5.2.3.tar.gz
 
 cd xz-5.2.3
 
