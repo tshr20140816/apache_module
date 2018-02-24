7c0a849b62c817cf1c1b658c13b701af3a341342
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-24 15:50:44 +0900

    Update post_install2.sh

--- post_install2.sh
+++ post_install2.sh
@@ -5,6 +5,12 @@ set -x
 date
 start_date=$(date)
 
+export HOME2=${PWD}
+export PATH="/tmp/usr/bin:${PATH}"
+
+export CFLAGS="-march=native -O2"
+export CXXFLAGS="$CFLAGS"
+
 cd /tmp
 
 wget http://ftp.gnu.org/pub/gnu/gettext/gettext-latest.tar.gz
@@ -12,6 +18,9 @@ wget http://ftp.gnu.org/pub/gnu/gettext/gettext-latest.tar.gz
 tar xf gettext-latest.tar.gz
 cd gettext*
 ./configure --help
+./configure --prefix=/tmp/usr
+make -j2
+make install
 
 exit
 
