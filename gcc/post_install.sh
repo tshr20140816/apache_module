87e2f4e7a80a36322a1edfd22bed24702ec159d7
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-25 20:23:16 +0900

    Update post_install.sh

--- gcc/post_install.sh
+++ gcc/post_install.sh
@@ -11,6 +11,39 @@ gcc --version
 
 whereis gcc
 
+# ***** postgresql *****
+
+postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
+postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
+postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
+
+echo ${postgres_user}
+echo ${postgres_password}
+echo ${postgres_server}
+echo ${postgres_dbname}
+
+export PGPASSWORD=${postgres_password}
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+CREATE TABLE t_files (
+  file_name character varying(255) NOT NULL
+ ,file_base64_text text NOT NULL
+);
+ALTER TABLE t_files ADD CONSTRAINT table_key PRIMARY KEY(file_name);
+__HEREDOC__
+cat /tmp/sql_result.txt
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+SELECT file_name
+      ,length(file_base64_text)
+  FROM t_files
+ ORDER BY file_name
+__HEREDOC__
+cat /tmp/sql_result.txt
+
+# ***** env *****
+
 export HOME2=${PWD}
 export PATH="/tmp/usr/bin:${PATH}"
 export LD_LIBRARY_PATH=/tmp/usr/lib
@@ -79,6 +112,16 @@ ls -Ralng
 cd /tmp
 
 time tar -jcf usr.tar.bz2 usr
+
+base64 -w 0 usr.tar.bz2 > usr.tar.bz2.base64.txt
+
+set +x
+base64_text=$(cat /tmp/ccache_cache.tar.bz2.base64.txt)
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+INSERT INTO t_files (file_name, file_base64_text) VALUES ('usr.tar.bz2', '${base64_text}');
+__HEREDOC__
+set -x
 
 ls -lang
 
