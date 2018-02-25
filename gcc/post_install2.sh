41ac8f648142149b7e3305d6c62ba85fe91c7f2e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-25 23:33:37 +0900

    Update post_install2.sh

--- gcc/post_install2.sh
+++ gcc/post_install2.sh
@@ -84,5 +84,7 @@ time ../configure --prefix=/tmp/usr --mandir=/tmp/man --docdir=/tmp/doc \
 time make -j${parallels}
 make install
 
+cat /tmp/gcc-7.3.0/work/config.cache
+
 echo ${start_date}
 date
