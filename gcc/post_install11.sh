574490d011ec5f762f1c20ccba945cf2556b7637
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-26 13:31:57 +0900

    Update post_install11.sh

--- gcc/post_install11.sh
+++ gcc/post_install11.sh
@@ -16,7 +16,7 @@ cd /tmp
 
 wget mirrors.kernel.org/ubuntu/pool/main/g/gcc-5/gcc-5_5.4.0-6ubuntu1~16.04.9_amd64.deb
 
-gdebi gcc-5_5.4.0-6ubuntu1~16.04.9_amd64.deb
+dpkg -i gcc-5_5.4.0-6ubuntu1~16.04.9_amd64.deb
 
 echo ${start_date}
 date
