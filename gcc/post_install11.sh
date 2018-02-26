246d381bfa6b5ab8a4649317fb3557bd516829fb
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-26 13:25:59 +0900

    Update post_install11.sh

--- gcc/post_install11.sh
+++ gcc/post_install11.sh
@@ -9,6 +9,8 @@ chmod 755 start_web.sh
 
 gcc --version
 
+# https://packages.ubuntu.com/xenial-updates/devel/gcc-5
+
 cd /tmp
 
 wget mirrors.kernel.org/ubuntu/pool/main/g/gcc-5/gcc-5_5.4.0-6ubuntu1~16.04.9_amd64.deb
