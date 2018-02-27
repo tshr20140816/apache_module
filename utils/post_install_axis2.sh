12b7f420ad3db83bf774abad7aa455b9610985aa
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-27 09:15:59 +0900

    Create post_install_axis2.sh

---
+++ utils/post_install_axis2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+date
+start_date=$(date)
+
+chmod 755 start_web.sh
+
+gcc --version
+pkg-config --help
+
+# ***** postgresql *****
+
+postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
+postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
+postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
+
+echo ${postgres_user}
+echo ${postgres_password}
+echo ${postgres_server}
+echo ${postgres_dbname}
+
+export PGPASSWORD=${postgres_password}
+
+# *****
+
+
+
+echo ${start_date}
+date
