95f5c7521ca973315213294ef06d88f843035c8c
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-27 14:18:29 +0900

    Update post_install_aria2.sh

--- utils/post_install_aria2.sh
+++ utils/post_install_aria2.sh
@@ -123,7 +123,7 @@ autoreconf -i
 
 ./configure --help
 # ./configure --prefix=/tmp/usr --mandir=/tmp/man --docdir=/tmp/doc ARIA2_STATIC=yes
-./configure --prefix=/tmp/usr --mandir=/tmp/man --docdir=/tmp/doc
+LIBARCHIVE_LIBS=/tmp/usr/lib/liblzma.a ./configure --prefix=/tmp/usr --mandir=/tmp/man --docdir=/tmp/doc
 time make -j2
 make install
 
