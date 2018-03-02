7a2a465bd830c7a14d88bc0db4a8bf6d5097fada
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-03 02:38:07 +0900

    Update post_install_0303.sh

--- utils/post_install_0303.sh
+++ utils/post_install_0303.sh
@@ -9,8 +9,15 @@ chmod 755 start_web.sh
 
 # *****
 
+cd /tmp
+
 wget https://github.com/aria2/aria2/releases/download/release-1.33.1/aria2-1.33.1.tar.bz2
 
 tar xvf aria2-1.33.1.tar.bz2
 
-find ./ -name aria2c -print
+cd aria2-1.33.1
+
+./configure --help
+./configure --prefix=/tmp/usr
+make -j2
+make install
