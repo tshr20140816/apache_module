b5be9c7c85ffc27760f7292d46dbd4efa87d47b1
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-02 11:03:53 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -15,24 +15,24 @@ printenv
 
 ls -lang
 
-export LD_LIBRARY_PATH=/tmp/usr/lib
+# export LD_LIBRARY_PATH=/tmp/usr/lib
 
 # chmod +x libbrotlicommon.so.1
 # chmod +x libbrotlienc.so.1
 
-mkdir -p /tmp/usr/lib
-cp libnghttp2.so.14 /tmp/usr/lib/
-cp libbrotlicommon.so.1 /tmp/usr/lib/
-cp libbrotlienc.so.1 /tmp/usr/lib/
+# mkdir -p /tmp/usr/lib
+# cp libnghttp2.so.14 /tmp/usr/lib/
+# cp libbrotlicommon.so.1 /tmp/usr/lib/
+# cp libbrotlienc.so.1 /tmp/usr/lib/
 
-ls -lang /tmp/usr/lib/
+# ls -lang /tmp/usr/lib/
 
-ldd /tmp/usr/lib/libbrotlicommon.so.1
-ldd /tmp/usr/lib/libbrotlienc.so.1
+# ldd /tmp/usr/lib/libbrotlicommon.so.1
+# ldd /tmp/usr/lib/libbrotlienc.so.1
 
-ldd ./mod_http2.so
-ldd ./mod_proxy_http2.so
-ldd ./mod_brotli.so
-ldd ./mod_cache_disk.so
+# ldd ./mod_http2.so
+# ldd ./mod_proxy_http2.so
+# ldd ./mod_brotli.so
+# ldd ./mod_cache_disk.so
 
 vendor/bin/heroku-php-apache2 -C apache.conf www
