d12566d261c873d8842d3dd2baa243d08b1ce1a8
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-05 08:59:22 +0900

    Update post_install.sh

--- pg/post_install.sh
+++ pg/post_install.sh
@@ -12,15 +12,23 @@ python --version
 
 cd /tmp
 
-wget https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v2.1/source/pgadmin4-2.1.tar.gz
+wget https://github.com/GrahamDumpleton/mod_wsgi/archive/4.6.2.tar.gz
 
-tar xf pgadmin4-2.1.tar.gz
+tar xf 4.6.2.tar.gz
 
-cd pgadmin4-2.1
+ls -lang
+
+cd /tmp
+
+# wget https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v2.1/source/pgadmin4-2.1.tar.gz
+
+# tar xf pgadmin4-2.1.tar.gz
+
+# cd pgadmin4-2.1
 
 # time make -j2
 
-ls -Rlang
+# ls -Rlang
 
 
 echo ${start_date}
