bf95460644b153d13e81584cc13ab221fd9aaf74
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-05 10:12:40 +0900

    Update post_install.sh

--- pg/post_install.sh
+++ pg/post_install.sh
@@ -35,15 +35,14 @@ find / -name mod_wsgi.so -print
 
 cp ${HOME2}/.heroku/php/libexec/mod_wsgi.so ${HOME2}/
 
-cd /tmp
+cd ${HOME2}
 
 wget https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v2.1/source/pgadmin4-2.1.tar.gz
 
 tar xf pgadmin4-2.1.tar.gz
+rm pgadmin4-2.1.tar.gz
 
 cd pgadmin4-2.1
-
-cp -r web ${HOME2}/www/
 
 echo ${start_date}
 date
