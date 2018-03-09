d092a33df953b62241526df8b78a247258cd25e5
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-10 04:37:56 +0900

    Update post_install.sh

--- expect/post_install.sh
+++ expect/post_install.sh
@@ -7,6 +7,19 @@ start_date=$(date)
 
 chmod 755 start_web.sh
 
+export PATH="/tmp/usr/bin:${PATH}"
+
+cd /tmp
+
+wget https://prdownloads.sourceforge.net/tcl/tcl8.6.8-src.tar.gz
+tar xf tcl8.6.8-src.tar.gz
+cd tcl*
+pwd
+./configure --help
+./configure --prefix=/tmp/usr
+time make
+make install
+
 cd /tmp
 
 wget https://jaist.dl.sourceforge.net/project/expect/Expect/5.45.4/expect5.45.4.tar.gz
