423f5967de3b41d139fabd03beabe11e4799af5f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-10 19:31:28 +0900

    Update post_install.sh

--- expect/post_install.sh
+++ expect/post_install.sh
@@ -7,10 +7,13 @@ start_date=$(date)
 
 chmod 755 start_web.sh
 
+export HOME2=${PWD}
 export PATH="/tmp/usr/bin:${PATH}"
 
 export CFLAGS="-march=native -O2"
 export CXXFLAGS="$CFLAGS"
+
+# ***** tcl *****
 
 cd /tmp
 
@@ -25,6 +28,8 @@ ls -lang
 time make -j2
 make install
 
+# ***** expect *****
+
 cd /tmp
 
 wget https://jaist.dl.sourceforge.net/project/expect/Expect/5.45.4/expect5.45.4.tar.gz
@@ -36,6 +41,19 @@ cd expect5.45.4
 ./configure --prefix=/tmp/usr --mandir=/tmp/man --docdir=/tmp/doc
 time make -j2
 make install
+
+# ***** heroku cli *****
+
+cd /tmp
+
+wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O heroku.tar.gz
+
+mkdir -m 777 ${HOME2}/heroku-cli
+
+tar xf heroku.tar.gz -C ${HOME2}/heroku-cli --strip=1
+
+
+# *****
 
 cd /tmp/usr
 
