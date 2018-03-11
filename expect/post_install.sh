cd4afe3c7eb263612cb50ecb900f01e6d7bbf81d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-11 23:52:36 +0900

    Update post_install.sh

--- expect/post_install.sh
+++ expect/post_install.sh
@@ -13,6 +13,50 @@ export PATH="/tmp/usr/bin:${PATH}"
 export CFLAGS="-march=native -O2"
 export CXXFLAGS="$CFLAGS"
 
+# ***** postgresql *****
+
+postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
+postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
+postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
+
+echo ${postgres_user}
+echo ${postgres_password}
+echo ${postgres_server}
+echo ${postgres_dbname}
+
+export PGPASSWORD=${postgres_password}
+
+cd /tmp
+
+mkdir -m 777 usr
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+SELECT file_base64_text
+  FROM t_files
+ WHERE file_name = 'usr_expect.tar.bz2'
+__HEREDOC__
+
+if [ $(cat /tmp/sql_result.txt | grep -c '(1 row)') -eq 1 ]; then
+  set +x
+  echo $(cat /tmp/sql_result.txt | head -n 3 | tail -n 1) > /tmp/usr.tar.bz2.base64.txt
+  set -x
+  base64 -d /tmp/usr.tar.bz2.base64.txt > /tmp/usr.tar.bz2
+  tar xf /tmp/usr.tar.bz2 -C /tmp/usr --strip=1
+  
+  /tmp/usr/bin/expect -c "
+set timeout 5
+exp_internal 1
+spawn /tmp/heroku-cli/bin/heroku info ${APP_NAME}
+expect \"Email:\"
+send \"${PARAM1}\n\"
+expect \"Password:\"
+send \"${PARAM2}\n\"
+expect \"$\"
+"
+  exit
+fi
+
 # ***** tcl *****
 
 cd /tmp
